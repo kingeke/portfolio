7a935d5f2689ebd3b0c09a8d7cd4e33b6365be0c
Chinonso Eke <Chinonsoeke@gmail.com>
2019-08-06 15:57:44 +0100

    Updates

---
+++ precache-manifest.0b300c24087fc85f9697cfc46055daa5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c657dd100dff0f85d151b351cbfef7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fd113b071e8ab093a316",
+    "url": "/portfolio/static/css/2.6a288956.chunk.css"
+  },
+  {
+    "revision": "fd113b071e8ab093a316",
+    "url": "/portfolio/static/js/2.8e9d6f97.chunk.js"
+  },
+  {
+    "revision": "380fb0d1c0f059c41010",
+    "url": "/portfolio/static/js/main.094248d7.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/portfolio/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/portfolio/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/portfolio/static/media/revicons.2feb69cc.eot"
+  }
+]);
