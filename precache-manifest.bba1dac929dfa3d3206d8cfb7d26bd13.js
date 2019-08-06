7305b8cdddc05aa6ae2c585e0fbefa612fc3b5fe
Chinonso Eke <Chinonsoeke@gmail.com>
2019-08-06 18:03:33 +0100

    Updates

---
+++ precache-manifest.bba1dac929dfa3d3206d8cfb7d26bd13.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f54f8f7af269355e3235cc384afff0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6060f28a107046f3a11",
+    "url": "/static/css/2.c3e2ae4f.chunk.css"
+  },
+  {
+    "revision": "c6060f28a107046f3a11",
+    "url": "/static/js/2.f8ea864d.chunk.js"
+  },
+  {
+    "revision": "4351e29c876adf5b678d",
+    "url": "/static/js/main.953870c7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.cab4feca33232fcbe921d40bf0204623.js"
+  "/precache-manifest.bba1dac929dfa3d3206d8cfb7d26bd13.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
