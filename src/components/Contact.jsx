3a9c337e76ca09edec42332382074c53ee04a009
Chinonso Eke <ceke@renmoney.com>
2025-04-02 00:54:21 +0100

    remove phone number from portfolio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,10 +10,9 @@ export default function Contact() {
                     <div className="col-12 my-auto p-5 text-center">
                         <h1>Contact Me</h1>
                         <h4>Interested in working with me? <Emoji symbol="😎" label="cool" /></h4>
-                        <h4>Send an email or contact me on WhatsApp <Emoji symbol="😉" label="wink" /></h4>
+                        <h4>Send me an email, let's connect! <Emoji symbol="😉" label="wink" /></h4>
                         <div className="d-flex justify-content-center gap-3">
                             <h5><i className="fas fa-envelope"></i> <a href="mailto:chinonsoeke@gmail.com" className="text-white">chinonsoeke@gmail.com</a></h5>
-                            <h5><i className="fas fa-phone fa-flip-horizontal"></i> +2347063944465</h5>
                         </div>
                         <h4>Remember, you're awesome !!!</h4>
                     </div>
